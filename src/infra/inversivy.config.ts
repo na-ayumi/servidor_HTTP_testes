ff577af6b7c7ca30f571aa98648b4f3f9034c96c
Natalia Ayumi Shimizu da Silva <nshimizudasilva@gmail.com>
2026-01-11 13:24:49 -0400

    Correção - Container

--- src/infra/inversivy.config.ts
+++ src/infra/inversivy.config.ts
@@ -2,11 +2,13 @@ import "reflect-metadata";
 import { Container } from "inversify";
 import { ILogger } from "../domain/ILogger";
 import { IMailer } from "../domain/IMailer";
+import { IReportService } from "../domain/IReportService";
 
 import { WinstonConsole } from "./logger/WinstonConsole";
 import { WinstonFile } from "./logger/WinstonFile";
 import { EtherealMailer } from "./mailer/EtherealMailer";
 import { SMTPMailer } from "./mailer/SMTPMailer";
+import { ReportService } from "../services/ReportService";
 
 import { TYPES } from "../types";
 
@@ -19,5 +21,7 @@ if (process.env.APP_ENV === "prod") {
     container.bind<ILogger>(TYPES.Logger).to(WinstonConsole).inSingletonScope;
     container.bind<IMailer>(TYPES.Mailer).to(EtherealMailer).inSingletonScope;
 }
+
+container.bind<IReportService>(TYPES.ReportService).to(ReportService);
 
 export {container};
